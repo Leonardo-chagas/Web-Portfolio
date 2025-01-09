b475bbf2f6a3faaf02d913ea34ca7d57acac09a2
leonardo-chagas <leoschagas@hotmail.com>
2025-01-08 21:10:32 -0300

    criou uma aparência básica para a página de projetos e adicionou parte dos textos de informação sobre os projetos

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -2,6 +2,10 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import React, { useLayoutEffect } from 'react'
 import "../css/Container.css"
+import "../css/Text.css"
+import "../css/Projects.css"
+import game from "../images/Game2.png"
+import planalto from "../images/planalto.jpeg"
 
 function Projects() {
     useLayoutEffect(() => {
@@ -10,6 +14,42 @@ function Projects() {
     return (
         <div id="bodyContainer">
             <Navbar current={3}/>
+            <div class="projectContent">
+                <div class="projectTextContent">
+                    <h2 class="titleText">Portfolio Web</h2>
+                    <h3 class="titleText">Tecnologia: <b>React</b></h3>
+                    <p class="infoText"></p>
+                </div>
+                <img></img>
+            </div>
+            <div class="projectContent">
+                <div class="projectTextContent">
+                    <h2 class="titleText">Aplicativo Mobile Para Compra de Passagem de Ônibus</h2>
+                    <h3 class="titleText">Tecnologia: <b>React Native</b></h3>
+                    <p class="infoText">Aplicativo que foi criado para a disciplina de Projeto e Desenvolvimento de Software II. 
+                        Se trata de aplicativo mobile, onde o usuário pode pesquisar e comprar passagens de ônibus.
+                        Foi um trabalho feito em grupo, onde eu trabalhei na parte de Front-end junto com uma colega do grupo, enquanto outro colega trabalhou no Back-end.</p>
+                </div>
+                <img src={planalto} alt="imagem" width={200} height={300}></img>
+            </div>
+            <div class="projectContent">
+                <div class="projectTextContent">
+                    <h2 class="titleText">Jogo de Cartas Para o Ensino de Programação</h2>
+                    <h3 class="titleText">Tecnologia: <b>Unity Engine</b></h3>
+                    <p class="infoText">Jogo criado para o meu TCC, com a utilização da Unity Engine para o seu desenvolvimento. 
+                        Se trata de um jogo de cartas com o objetivo de promover o aprendizado de programação. 
+                        Uma linguagem de programação simples foi crada com ANTLR, para validar os códigos criados no jogo.</p>
+                </div>
+                <img src={game} alt="imagem" width={300} height={200}></img>
+            </div>
+            <div class="projectContent">
+                <div class="projectTextContent">
+                    <h2 class="titleText"></h2>
+                    <h3 class="titleText">Tecnologia: <b>Python</b></h3>
+                    <p class="infoText"></p>
+                </div>
+                <img></img>
+            </div>
             <Footer/>
         </div>
     );
