f9ec2ea966110dc73c9bec2d4e5c81a3c2e560fb
leonardo-chagas <leoschagas@hotmail.com>
2025-04-22 16:44:01 -0300

    mudou o raio de borda da minha foto, adicionou animação para o título da página, mudou a cor roxa para um lilás, adicionou uma coloração mais clara quando o mouse está em cima dos botões de seleção de linguagem

--- portfolio/src/components/Title.js
+++ portfolio/src/components/Title.js
@@ -1,21 +1,32 @@
 import "../css/Title.css"
 import { useTranslation } from "react-i18next"
 
+import { motion } from "framer-motion";
+
 function Title() {
     const {t} = useTranslation();
     const titleTrans = t('Title');
 
     return (
         <div id="titleContainer">
-            <div class="titleContent">
-                <h1 id="firstName">Leonardo</h1>
-                <h2 id="lastName">Chagas</h2>
-            </div>
-            <div class="titleContent">
-                <h3 class="titles">{titleTrans.title1}</h3>
-                <h3 class="titles">{titleTrans.title2}</h3>
-                <h3 class="titles">{titleTrans.title3}</h3>
-            </div>
+            <motion.div class="titleContent" animate={{x:0}} initial={{x:-300}} transition={{duration:1.5}}>
+                {/* <motion.div animate={{x:0}} initial={{x:-300}} transition={{duration:3}}> */}
+                    <h1 id="firstName">Leonardo</h1>
+                {/* </motion.div>
+                <motion.div animate={{x:0}} initial={{x:-300}} transition={{duration:3}}> */}
+                    <h2 id="lastName">Chagas</h2>
+                {/* </motion.div> */}
+                
+            </motion.div>
+            <motion.div class="titleContent" animate={{x:0}} initial={{x:300}} transition={{duration:1.5}}>
+                {/* <motion.div animate={{x:0}} initial={{x:100}}> */}
+                    <h3 class="titles">{titleTrans.title1}</h3>
+                    <h3 class="titles">{titleTrans.title2}</h3>
+                {/* </motion.div>
+                <motion.div animate={{x:0}} initial={{x:100}}> */}
+                    <h3 class="titles">{titleTrans.title3}</h3>
+                {/* </motion.div> */}
+            </motion.div>
         </div>
     );
 }
